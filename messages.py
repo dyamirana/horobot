c9257514f6e4ab36d4830fe449c935f774f54d3e
dyamirana <danil12b@gmail.com>
2017-06-26 00:10:12 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -9,11 +9,11 @@ def answers(helper,text,user_id,attachments,message):
     if len(utils.getusers(id=user_id)) > 0:
         if utils.getusers(id=user_id)[0]['member']==1:
             helper.send_message(user_id=user_id,message=hotoscope.returnHoroScope(id=user_id,zod=text.lower()))
-    else:
-        if helper.api.groups.isMember(group_id=settings.group_id,user_id=user_id)==1:
-            helper.send_message(user_id=user_id, message=hotoscope.returnHoroScope(id=user_id, zod=text.lower()))
         else:
-            helper.send_message(user_id=user_id, message=settings.joinpls)
+            if helper.api.groups.isMember(group_id=settings.group_id,user_id=user_id)==1:
+                helper.send_message(user_id=user_id, message=hotoscope.returnHoroScope(id=user_id, zod=text.lower()))
+            else:
+                helper.send_message(user_id=user_id, message=settings.joinpls)
 
 class MessageModule:
     def __init__(self,api):
