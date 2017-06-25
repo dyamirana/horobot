8e4f18481450a01e01cd8145f71b7c70ea2789fe
dyamirana <danil12b@gmail.com>
2017-06-25 23:31:25 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ if 'appname' in os.environ:
     host = 'https://'+os.environ.get('appname')+'.herokuapp.com'
 else:
     host = 'http://vps.kadabot.tk'
-group_id=int(os.environ.get('token',141410969))
+group_id=int(os.environ.get('groupid',141410969))
 
 token = os.environ.get('token','740f3f24062cf595ba12a8e5862c270814f4a5d7d7ab2a4001c5c45438d1881dea2d3d79124b4d0b670aa')
 debug=True
