0eb328e9f7996a9e3f469949ad7ad6f60c93f542
dyamirana <danil12b@gmail.com>
2017-07-06 23:09:12 +0300

    Update joinleave.py

--- joinleave.py
+++ joinleave.py
@@ -9,17 +9,16 @@ class JoinLeave:
         self.api = api
         self.functions = apifunc.Functions(api)
         self.send_message = self.functions.send_message
-        if settings.db:
-            self.db = utils.DB('users')
+
 
     def join(self,user):
         try:
             if settings.join:
                 user_id = user.user_id
-                if settings.db:
-                    self.db.insert_user(id=user_id,req={'date':time.time()+settings.dispatch_timer,'data2':'day1','subscription':1})
+                if len(utils.getusers(id=user_id)) < 1:
+                    utils.insert_users(id=user_id,member=1,mesallow=0,time=0,game=0,lives=settings.lives,score=0,pics='[]',answer='')
                 else:
-                    utils.insert_users(id=user_id,member=1,mesallow=0,time=0)
+                    utils.insert_users(id=user_id, member=1)
                 if self.api.messages.isMessagesFromGroupAllowed(group_id=settings.group_id,user_id=user_id)['is_allowed'] == 1:
                     utils.insert_users(id=user_id, mesallow=1)
                     self.send_message(user_id=user_id,message=settings.join_mes)
@@ -30,10 +29,10 @@ class JoinLeave:
         try:
             if settings.leave:
                 user_id = user.user_id
-                if settings.db:
-                    self.db.insert_user(id=user_id,req={'date':time.time()+settings.dispatch_timer,'member':0})
+                if len(utils.getusers(id=user_id)) < 1:
+                    utils.insert_users(id=user_id,member=0,mesallow=0,time=0,game=0,lives=settings.lives,score=0,pics='[]',answer='')
                 else:
-                    utils.insert_users(id=user_id,member=0,mesallow=0,time=0)
+                    utils.insert_users(id=user_id,member=0,mesallow=0)
                 if self.api.messages.isMessagesFromGroupAllowed(group_id=settings.group_id,user_id=user_id)['is_allowed'] == 1:
                     utils.insert_users(id=user_id, mesallow=1)
                     self.send_message(user_id=user_id,message=settings.leave_mes)
