226a63a108c30aa1053164bd08a455e012b949a0
dyamirana <danil12b@gmail.com>
2017-07-06 18:38:56 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,19 +7,9 @@ from plugins import hotoscope
 def answers(helper,text,user_id,attachments,message):
     ### PUT UR ANSWERS HERE ###
 
-    if len(utils.getusers(id=user_id)) > 0:
 
-        if utils.getusers(id=user_id)[0]['member']==1:
-            helper.send_message(user_id=user_id,message=hotoscope.returnHoroScope(id=user_id,zod=text.lower()))
-        else:
-            helper.send_message(user_id=user_id, message=settings.joinpls)
-    else:
-        if helper.api.groups.isMember(group_id=settings.group_id,user_id=user_id)==1:
-            utils.insert_users(id=user_id,member=1,mesallow=1,time=0)
-            helper.send_message(user_id=user_id, message=hotoscope.returnHoroScope(id=user_id, zod=text.lower()))
-        else:
-            utils.insert_users(id=user_id, member=0, mesallow=1, time=0)
-            helper.send_message(user_id=user_id, message=settings.joinpls)
+    helper.send_message(user_id=user_id, message=hotoscope.returnHoroScope(id=user_id, zod=text.lower()))
+
 
 class MessageModule:
     def __init__(self,api):
